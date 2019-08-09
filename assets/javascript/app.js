7c84c9de925c88471c8ca1878b0e3f49916b18ac
Peter Geyser <peterpgeyser@gmail.com>
2019-08-09 13:54:14 -0700

    Submit button functionality complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,10 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
+$(document).ready(function () {
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
     apiKey: "AIzaSyCsldtHwaBA2oilsHADtkpgD0ngferxXkY",
     authDomain: "train-schedule-840bf.firebaseapp.com",
-    databaseURL: "https://train-schedule-840bf.firebaseio.com/",
+    databaseURL: "https://train-schedule-840bf.firebaseio.com",
     projectId: "train-schedule-840bf",
     storageBucket: "",
     messagingSenderId: "258461138286",
@@ -10,20 +12,41 @@ var firebaseConfig = {
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-
-// Create a variable to reference the database.
-var database = firebase.database();
-
- //Submit button on click
-  //cancel default behavior so that page doesn't reset
-
-  //grab values from each of the input fields
-
-    //Make vars for train name, destination, and frequency
-
+  
+  // Create a variable to reference the database.
+  var database = firebase.database();
+  
+  var name = "";
+  var destination = "";
+  var frequency = 0;
+  var nextArrival = "";
+  var minsAway = 0;
+  var firstTrainTime = "";
+  
+  
+  //Submit button on click
+  $("#submit").on("click", function () {
+  
+    console.log("I've been clicked!");
+    
+    // grab values from each of the input fields
+    name = $("#trainName").val().trim();
+    destination = $("#trainDestination").val().trim();
+    frequency = $("#trainFrequency").val().trim();
+  
+    firstTrainTime = $("#trainTime").val().trim();
+  
+    console.log(name);
+    console.log(destination);
+    console.log(frequency);
+    console.log(firstTrainTime);
+  
     //Use moment.js to calcuate train's next arrival and minutes away
-    //make vars for these values
-
+  
     //set all 5 of these vars in firebase
+  
+    //print all train schedule table data using firebase
+  
+  })
 
-  //print all train schedule table data using firebase
+})
